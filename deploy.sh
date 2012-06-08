37ec5bd75747b90a9947ce7c7082c4417d270ad2
Wlodek Bzyl <matwb@univ.gda.pl>
2012-06-08 21:27:52 +0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh wbzyl@tao.inf.ug.edu.pl "cd /srv/www/pspi ; git pull; touch /srv/www/pspi/tmp/restart.txt"
+ssh wbzyl@tao.inf.ug.edu.pl "cd /srv/www/pspi ; git pull; rake assets:precompile; touch /srv/www/pspi/tmp/restart.txt"
