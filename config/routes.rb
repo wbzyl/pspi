8a7afc2bde6ee6c03ce7133458825df576e8ecfb
Wlodek Bzyl <matwb@univ.gda.pl>
2015-09-14 19:50:25 +0200

    add terminarz_07 to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Pspi::Application.routes.draw do
   match '/terminarz_04', to: 'pages#terminarz_04'
   match '/terminarz_05', to: 'pages#terminarz_05'
   match '/terminarz_06', to: 'pages#terminarz_06'
+  match '/terminarz_07', to: 'pages#terminarz_07'
   match '/program', to: 'pages#program'
   match '/informacje', to: 'pages#informacje'
   match '/zapisy', to: 'pages#zapisy'
